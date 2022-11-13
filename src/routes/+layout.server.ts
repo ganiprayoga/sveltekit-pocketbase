39daf34e31e6cc453c69914656f59bc5c6624178
ganiprayoga <gani.prayoga@gmail.com>
2022-11-14 06:24:27 +0700

    Setup & User Management

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,9 @@
+export const load = ({ locals }) => {
+  if (locals.user) {
+    return { user: locals.user }
+  };
+
+  return {
+    user: undefined
+  };
+}
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
+
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true
+		}
+	},
+	optimizeDeps: {
+		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+	}
+};
+
+export default config;
